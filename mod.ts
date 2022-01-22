c711446806589f99a38d8a12bcc76ded432b2fd1
Hal Blackburn <hwtb2@cam.ac.uk>
2022-01-22 12:48:32 +0000

    feat: export public API via mod.ts

---
+++ mod.ts
@@ -0,0 +1,10 @@
+// Copyright 2021-2022 Hal Blackburn. All rights reserved. MIT license.
+export const VERSION = "0.0.0"; // managed by standard-version, do not modify
+export {
+  ShutdownAwareTransformStream,
+} from "./shutdown_aware_transform_stream.ts";
+export type {
+  ShutdownAwareTransformer,
+  ShutdownAwareTransformStreamController,
+  ShutdownAwareTransformStreamOptions,
+} from "./shutdown_aware_transform_stream.ts";
